1eea9b12cbdd5d95c5a54ef8efc7e62e258c970d
Shilpi Verma <shilpiverma@Shilpis-MacBook-Pro.local>
2018-03-20 10:08:25 -0500

    setup dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = (env) => {
         //you want to output that web pack file
         //joining absolute path with the local path to the public folder
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public','dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -46,7 +46,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath:'/dist/'
     }
   };
 };
